67a1dcb8d445cf5249b1d2f6c4325b53435cc897
ViswaVMC <viswa.vmc25@gmail.com>
2018-12-16 16:44:14 +0530

    test project and config transform

---
+++ ThirdPartyProvider/Tests/ThirdPartyProvider.Tests/ThirdPartyControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThirdPartyProvider.Controllers;
+
+namespace ThirdPartyProvider.Tests
+{
+    [TestClass]
+    public class ThirdPartyControllerTest
+    {
+        [TestMethod]
+        public void ThirdPartyController_Ping_Success()
+        {
+            var thirdPartController = new ThirdPartyController();
+
+            var response = thirdPartController.Get();
+
+            Assert.IsNotNull(response);
+        }
+    }
+}
